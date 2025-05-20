8ede7fef9ce533d5e71e13bb48e3468a6c5ae69d
juanaperaita <juanaperai26@gmail.com>
2025-05-20 20:10:33 +0200

    ✨ Modify IO Models to get all information from API

--- backend/io_models.py
+++ backend/io_models.py
@@ -1,9 +1,31 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class NameRequest(BaseModel):
     name: str
 
+class OriginPlanet(BaseModel):
+    id: int
+    name: str
+    isDestroyed: bool
+    description: Optional[str] = None
+    image: Optional[str] = None
+    deletedAt: Optional[str] = None
+
+class Character(BaseModel):
+    id: int
+    name: str
+    ki: Optional[str] = None
+    maxKi: Optional[str] = None
+    race: Optional[str] = None
+    gender: Optional[str] = None
+    description: Optional[str] = None
+    image: Optional[str] = None
+    affiliation: Optional[str] = None
+    deletedAt: Optional[str] = None
+    originPlanet: Optional[OriginPlanet] = None
+
 class ApiResponse(BaseModel):
     original: str
     processed: str
-    names: list[str]
+    names: list[Character]
